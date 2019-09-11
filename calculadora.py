7578e1ba5e082a35eb3a69b966901927a10944d0
DenissoHimself <imeiol@mail.com>
2019-09-11 17:54:33 -0300

    Contribuição de DenissoHimself, turma 524

--- calculadora.py
+++ calculadora.py
@@ -1,16 +1,15 @@
-#Complete as funcoes a seguir
-
 def soma(a, b):
-	#Insira o codigo aqui
-
+        #Insira o codigo aqui
+    print("A soma é: ",a+b)
 def subtrai(a, b):
-	#Insira o codigo aqui
-
+        #Insira o codigo aqui
+    print("A subtração é: ",a-b)
 def multiplica(a, b):
-	#Insira o codigo aqui
-
+        #Insira o codigo aqui
+    print("A multiplicação é: ",a*b)
 def divide(a, b):
-	#Insira o codigo aqui
+        #Insira o codigo aqui
+    print ("A divisão é: ",a/b)
 
 
 #Programa principal
@@ -20,8 +19,17 @@ print("Calculadora simples")
 num1 = float(input("Insira o primeiro numero: "))
 num2 = float(input("Insira o segundo numero: "))
 
-soma(num1, num2)
-subtrai(num1, num2)
-multiplica(num1, num2)
-divide(num1, num2)
+if num2 != 0:
+    soma(num1, num2)
+    subtrai(num1, num2)
+    multiplica(num1, num2)
+    divide(num1, num2)
+else:
+    print("o segundo número não pode ser 0")
+
+
+
+
+
+
 
